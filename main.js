5a6ed88d9a7b25d834038fac30cda9ac11a38860
leilaCGR <152940142+leilaCGR@users.noreply.github.com>
2023-12-13 21:02:08 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,1 @@
-let app = document.getElementById('typewriter');
- 
-let typewriter = new Typewriter(app, {
-  loop: true,
-  delay: 75,
- cursor: "<span style='color: #A48DE2;'>|</span>",
-});
- 
-typewriter
-  .pauseFor(2500)
-  .typeString('<span style="color: #A48DE2;">Designer and Front end </span>')
-  .pauseFor(200)
-  .deleteChars(10)
-  .start();
+
